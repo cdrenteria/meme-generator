4764f6d32f7cb3137072a84f0bba491f29727c08
Chandler Renteria <69171143+cdrenteria@users.noreply.github.com>
2022-08-09 09:10:02 -0600

    added text functionality
    +created state object for the meme with toptext bottomtext and meme image
    +created form with controlled components
    +inserted state variables in jsx tags to display text
    +css for meme image and text position

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -21,16 +21,30 @@ export default function Meme(){
         })
     }
 
+    function handleChange(event){
+        const {name, value, type} = event.target
+        setMeme(prevState => {
+            return{
+                ...prevState,
+                [name]:value
+            }
+        })
+    }
+    console.log(meme)
     return(
         <div className="main-content">
             <form className="meme-form">
                 <div className="meme-inputs">
-                    <input/>
-                    <input /> 
+                    <input name="topText" value={meme.topText} onChange={handleChange}/>
+                    <input name="bottomText" value={meme.bottomText} onChange={handleChange}/> 
             </div>
             </form>
             <button className="new-img-btn" onClick={getMemeImage}>Get new meme image</button>
-            <img className="meme-img" src={meme.randomImage} />
+            <div className="meme">
+                <img className="meme-img" src={meme.randomImage} />
+                <h2 className="meme-text top">{meme.topText}</h2>
+                <h2 className="meme-text bottom">{meme.bottomText}</h2>
+            </div>
         </div>
     )
 }
